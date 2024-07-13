5a8a1166f52868765004ace4edb1eafac9bb6835
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-07-13 11:54:54 +0600

    product interface created

---
+++ src/modules/product/product.interface.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  name: string;
+  category: string;
+  brand: string;
+  price: number;
+  image: string;
+  description: string;
+  quantity: number;
+  rating: number;
+}
