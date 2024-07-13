dc065e46f982ef9ed9dcda8e9d9c6bc1e337304b
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-07-13 11:59:58 +0600

    product model created

---
+++ src/modules/product/product.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+import { IProduct } from './product.interface';
+
+const productSchema = new Schema<IProduct>({
+  name: {
+    type: String,
+    required: [true, 'Product name is required'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Product category is required'],
+  },
+  brand: {
+    type: String,
+    required: [true, 'Product brand is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Product image is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Product description is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Product quantity is required'],
+  },
+  rating: {
+    type: Number,
+    required: [true, 'Product rating is required'],
+  },
+});
+
+export const Product = model<IProduct>('Product', productSchema);
